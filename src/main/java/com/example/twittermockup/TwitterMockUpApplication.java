3703bf51bdb688f75d2c5f108a826bcb3d8b4bb0
Diana Gliga <diana.gliga02@e-uvt.ro>
2023-04-26 18:15:57 +0300

    first commit

---
+++ src/main/java/com/example/twittermockup/TwitterMockUpApplication.java
@@ -0,0 +1,15 @@
+package com.example.twittermockup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterMockUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterMockUpApplication.class, args);
+		System.out.println("Hello World!");
+		System.out.println("Today is a beautiful day, indeed");
+	}
+
+}
---
+++ src/test/java/com/example/twittermockup/TwitterMockUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twittermockup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterMockUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
