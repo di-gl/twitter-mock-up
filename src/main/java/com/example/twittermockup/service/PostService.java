d4e6930842918f2d453ad3136e8168a62401f262
Diana Gliga <diana.c.gliga@gmail.com>
2023-05-10 10:53:52 +0300

    declared Post interface methods

--- src/main/java/com/example/twittermockup/service/PostService.java
+++ src/main/java/com/example/twittermockup/service/PostService.java
@@ -5,9 +5,9 @@ import com.example.twittermockup.model.Post;
 import java.util.List;
 
 public interface PostService {
-    List<Post> getAllPost();
+    List<Post> getAllPosts();
     Post getPostById(Integer id);
     void registerPost(Post post);
     void updatePost(Integer id, Post post);
-    void delete(Integer id);
+    void deletePost(Integer id);
 }
