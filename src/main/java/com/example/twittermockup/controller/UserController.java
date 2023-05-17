59b54314c8f80df10002ae96db9f484503ef19d5
Diana Gliga <diana.c.gliga@gmail.com>
2023-05-17 12:06:38 +0300

    implemented register, search and follow functionalities

--- src/main/java/com/example/twittermockup/controller/UserController.java
+++ src/main/java/com/example/twittermockup/controller/UserController.java
@@ -12,42 +12,66 @@ import java.util.Map;
 
 @RestController
 public class UserController {
-
     @Autowired
     private UserService userService;
 
+    /* Get all users */
     @GetMapping(value = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    /* Get user by id */
     @GetMapping(value = "/users/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public User getUserById(@PathVariable Integer id) {
         return userService.getUserById(id);
     }
 
+    /* Get user by id using @RequestParam */
     @GetMapping(value = "/users/byParam", produces = MediaType.APPLICATION_JSON_VALUE)
     public User getUserByParam(@RequestParam Integer id) {
         return userService.getUserById(id);
     }
 
-    @PostMapping(value = "/users", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void registerUser(@RequestBody User user) {
-        userService.registerUser(user);
-    }
-
+    /* Update user by id */
     @PutMapping(value = "/users/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void updateUser(@PathVariable Integer id, @RequestBody User user) {
         userService.updateUser(id, user);
     }
 
+    /* Patch user by id */
     @PatchMapping(value = "/users/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void patchUser(@PathVariable Integer id, @RequestBody Map<String, String> partialUser) {
         userService.patchUser(id, partialUser);
     }
 
+    /* Delete user by id */
     @DeleteMapping(value = "/users/{id}")
     public void deleteUser(@PathVariable Integer id) {
         userService.deleteUser(id);
+    }
+
+    /* Register user by unique username */
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void registerUser(@RequestBody User user) {
+        userService.registerUser(user);
+    }
+
+    /* Search by username, first name or last name */
+    @GetMapping(value = "/search/{searchedString}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<User> searchUser(@PathVariable String searchedString) {
+        return userService.searchUser(searchedString);
+    }
+
+    /* Follow */
+    @PostMapping(value = "/{username}/follow/{usernameToBeFollowed}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void followUser(@PathVariable String username, @RequestBody User userToBeFollowed) {
+        userService.followUser(username, userToBeFollowed.getUsername());
+    }
+
+    /* Get user following */
+    @GetMapping(value = "/{username}/follows", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<String> getFollowing(@PathVariable String username) {
+        return userService.getWhoUserFollows(username);
     }
 }
