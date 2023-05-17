5808fc55b500e82f167a3bd70c55a32c982fd8fb
Diana Gliga <diana.c.gliga@gmail.com>
2023-05-17 12:05:50 +0300

    declared and implemented methods in PostService and PostServiceImpl

--- src/main/java/com/example/twittermockup/service/PostService.java
+++ src/main/java/com/example/twittermockup/service/PostService.java
@@ -2,12 +2,16 @@ package com.example.twittermockup.service;
 
 import com.example.twittermockup.model.Post;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PostService {
     List<Post> getAllPosts();
     Post getPostById(Integer id);
-    void registerPost(Post post);
+    void registerPost(String username, Post post);
     void updatePost(Integer id, Post post);
     void deletePost(Integer id);
+    List<Post> getUserPosts(String username);
+    List<Post> getUserPostsByParam(String username, LocalDateTime timestamp);
+    List<Post> getUserFeed(String username);
 }
--- src/main/java/com/example/twittermockup/service/PostServiceImpl.java
+++ src/main/java/com/example/twittermockup/service/PostServiceImpl.java
@@ -1,19 +1,26 @@
 package com.example.twittermockup.service;
 
+import com.example.twittermockup.advice.exception.PostNotFoundException;
 import com.example.twittermockup.model.Post;
+import com.example.twittermockup.model.User;
 import com.example.twittermockup.repository.PostRepository;
+import com.example.twittermockup.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PostServiceImpl implements PostService {
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public PostServiceImpl(PostRepository postRepository) {
+    public PostServiceImpl(PostRepository postRepository, UserRepository userRepository) {
         this.postRepository = postRepository;
+        this.userRepository = userRepository;
     }
 
     public List<Post> getAllPosts() {
@@ -24,8 +31,8 @@ public class PostServiceImpl implements PostService {
         return postRepository.getPostById(id);
     }
 
-    public void registerPost(Post post) {
-        postRepository.createPost(post);
+    public void registerPost(String username, Post post) {
+        postRepository.createPost(username, post);
     }
 
     public void updatePost(Integer id, Post post) {
@@ -34,5 +41,48 @@ public class PostServiceImpl implements PostService {
 
     public void deletePost(Integer id) {
         postRepository.deletePost(id);
+    }
+
+    public List<Post> getUserPosts(String username) {
+        List<Post> allPosts = postRepository.getAllPosts();
+        List<Post> userPosts = new ArrayList<>();
+        for (Post post : allPosts) {
+            if (post.getPostAuthor().getUsername().equals(username)) {
+                userPosts.add(post);
+            }
+        }
+        if (userPosts.isEmpty()) {
+            throw new PostNotFoundException(String.format("User \"%s\" has made no posts up until now", username));
+        }
+        return userPosts;
+    }
+
+    public List<Post> getUserPostsByParam(String username, LocalDateTime timestamp) {
+        List<Post> userPosts = getUserPosts(username);
+        List<Post> userPostsFilteredByTimestamp = new ArrayList<>();
+        for (Post post : userPosts) {
+            if (post.getPublishedDate().isBefore(timestamp)) {
+                userPostsFilteredByTimestamp.add(post);
+            }
+        }
+        if (userPostsFilteredByTimestamp.isEmpty()) {
+            throw new PostNotFoundException(String.format("User \"%s\" has made no posts before the timestamp %s", username, timestamp.toString()));
+        }
+        return userPostsFilteredByTimestamp;
+    }
+
+    public List<Post> getUserFeed(String username) {
+        User user = userRepository.getUserByUsername(username);
+        List<Integer> whoUserFollows = userRepository.getWhoUserFollows(user.getUserId());
+        List<Post> allPosts = postRepository.getAllPosts();
+        List<Post> feedPosts = new ArrayList<>();
+        for (Integer userId : whoUserFollows) {
+            for (Post post : allPosts) {
+                if (post.getPostAuthor().getUsername().equals(userRepository.getUserById(userId).getUsername())) {
+                    feedPosts.add(post);
+                }
+            }
+        }
+        return feedPosts;
     }
 }
